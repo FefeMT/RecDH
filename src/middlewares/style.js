0fbe1e4a0b48f05e44c2eb8c321a069996c63351
Felipe Martinez Tanoira <FelipeMTanoira@Felipes-MacBook-Air.local>
2022-12-14 13:50:25 -0300

    Login y Register no responsive

--- src/middlewares/style.js
+++ src/middlewares/style.js
@@ -3,13 +3,10 @@ let middleware = (req,res,next) => {
     let style;
     if (req.path.length == 1){
         style = "home"
-        console.log(style)
     } else if (req.path.length > 1 && ruta[1] == "users"){
         style = ruta[2]
-        console.log(style)
     } else {
         style = ruta[1]
-        console.log(style);
     }
     res.locals.style = style;
     return next()
